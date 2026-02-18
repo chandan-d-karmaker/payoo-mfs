83347aeaed3a1e1b94b90681b0fbb72d52aeeb8e
Chandan D Karmaker <chandankarmaker555@gmail.com>
2026-02-18 16:39:52 +0600

    Implementing cashout functionality

---
+++ script/cashout.js
@@ -0,0 +1,40 @@
+document.getElementById("btn-cashout").addEventListener("click", function()
+{
+    // get agent number
+    const agentNum = document.getElementById("agent-number");
+    const agentNumValue = agentNum.value;
+    // console.log(agentNumValue);
+
+    // get cashout amount
+    const cashoutMoney = document.getElementById("cashout-money");
+    const cashOutValue = cashoutMoney.value;
+    console.log(cashOutValue);
+
+    // get current balance
+    const balanceEle = document.getElementById("balance");
+    const balance = balanceEle.innerText;
+    console.log(balance);
+
+    // calculate new balance
+    const newBalance = Number(balance) - Number(cashOutValue);
+  
+    if(newBalance<0){
+        alert("Insufficient Amount");
+        return;
+    }
+
+    // get pass and verify
+    const userPass = document.getElementById('user-pass');
+    const pass = userPass.value;
+
+    if(pass == '115912'){
+        alert("CashOut successful");
+        console.log("New balance: ", newBalance);
+        balanceEle.innerText = newBalance;
+
+    } else{
+        alert("Invalid Pin!");
+        return;
+    }
+    
+})
