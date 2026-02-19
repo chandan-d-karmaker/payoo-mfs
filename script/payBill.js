3f00e8452d5333e1306bc5b2728e235cde28771a
Chandan D Karmaker <chandankarmaker555@gmail.com>
2026-02-19 11:24:37 +0600

    Designed Pay bill section

---
+++ script/payBill.js
@@ -0,0 +1,36 @@
+document.getElementById("btn-cashout").addEventListener("click", function()
+{
+    // get agent number and verify
+    const agentNumValue = getValueOfInput("agent-number");
+    if(agentNumValue.length != 11 ){
+        alert("Invalid Agent Number!");
+        return;
+    }
+
+    // get cashout amount
+    const cashOutValue = getValueOfInput("cashout-money");
+
+    // get current balance
+    const currentBalance = getBalance();
+
+    // calculate new balance
+    const newBalance = currentBalance - Number(cashOutValue);
+  
+    if(newBalance<0){
+        alert("Insufficient Amount");
+        return;
+    }
+
+    // get pass and verify
+    const pass = getValueOfInput("user-pass");
+
+    if(pass == '115912'){
+        alert("CashOut successful");
+        setBalance(newBalance);
+
+    } else{
+        alert("Invalid Pin!");
+        return;
+    }
+    
+})
